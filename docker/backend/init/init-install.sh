0b7459ad2b61c8c9afddf880cd9c4247cb9ea637
EmilioCMiranda <cecm2893@gmail.com>
2023-07-11 12:50:37 -0600

    API Rest + Imagen con ambiente para UI

--- docker/backend/init/init-install.sh
+++ docker/backend/init/init-install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+#Arrancando el servidor de solr
 echo -e 'rdsp4c3\n' | su root -c 'service solr start'
 
-cd /home/dspace/dspace-cris/dspace/target/dspace-installer && ant fresh_install
-/dspace/bin/dspace database migrate
-/dspace/bin/dspace database info
-cp -r /dspace/webapps/* /home/dspace/tomcat9/webapps/
+#Si el directorio de instalacion está vacío, se procede a instalar
+if [ -z "$(ls -A /dspace)" ]; then
 
-echo -e 'rdsp4c3\n' | su root -c 'cp -r /dspace/solr/* /var/solr/data/'
-echo -e 'rdsp4c3\n' | su root -c 'chown -R solr:solr /var/solr/data/'
-echo -e 'rdsp4c3\n' | su root -c 'service solr restart'
+     cd /home/dspace/dspace-cris/dspace/target/dspace-installer && ant fresh_install
+     /dspace/bin/dspace database migrate
+     /dspace/bin/dspace database info
+     cp -r /dspace/webapps/* /home/dspace/tomcat9/webapps/
 
-tail -f /dev/null
+     echo -e 'rdsp4c3\n' | su root -c 'cp -r /dspace/solr/* /var/solr/data/'
+     echo -e 'rdsp4c3\n' | su root -c 'chown -R solr:solr /var/solr/data/'
+     echo -e 'rdsp4c3\n' | su root -c 'service solr restart'
+     #Arrancando tomcat para hacer el primer deploy
+     /home/dspace/tomcat9/bin/catalina.sh run
+else
+     #Si la instalación se realizó, se hace un inicio simple de Tomcat
+     /home/dspace/tomcat9/bin/startup.sh
+fi
+
